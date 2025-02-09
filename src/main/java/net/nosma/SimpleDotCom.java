32a4209b5804e613f459495503d3e713d4cd361d
redEkim <mikhail6886@yandex.ru>
2025-02-09 20:35:24 +0500

    added tests and real code

--- src/main/java/net/nosma/SimpleDotCom.java
+++ src/main/java/net/nosma/SimpleDotCom.java
@@ -1,4 +1,31 @@
 package net.nosma;
 
 public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    String checkYourself(String userGuess) {
+        int guess = Integer.parseInt(userGuess);
+        String result = "Мимо";
+
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "Попал";
+                numOfHits++;
+                break;
+            }
+        }
+
+        if (numOfHits == locationCells.length) {
+            result = "Потопил";
+        }
+
+        System.out.println(result);
+
+        return result;
+    }
+
+    void setLocationCells(int[] loc) {
+
+    }
 }
