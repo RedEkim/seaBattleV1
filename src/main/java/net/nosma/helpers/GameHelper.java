4280613213b98484d9fec7e98c4df1e0a207ce24
redEkim <mikhail6886@yandex.ru>
2025-02-12 08:45:34 +0500

    improved the class GameHelper

--- src/main/java/net/nosma/helpers/GameHelper.java
+++ src/main/java/net/nosma/helpers/GameHelper.java
@@ -1,12 +1,21 @@
 package net.nosma.helpers;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class GameHelper {
-    public static String getUserInput(String text) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(text);
+    public static String getUserInput(String promt) {
 
-        return scanner.nextLine();
+        String inputLine = null;
+        System.out.print(promt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) return null;
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+        }
     }
-}
