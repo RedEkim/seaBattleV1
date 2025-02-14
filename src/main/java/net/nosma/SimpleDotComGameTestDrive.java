d701fa367aa25d4a84009f445b3aad15b9b76545
redEkim <mikhail6886@yandex.ru>
2025-02-14 09:12:47 +0500

    improved the class SimpleDotComGameTestDrive

--- src/main/java/net/nosma/SimpleDotComGameTestDrive.java
+++ src/main/java/net/nosma/SimpleDotComGameTestDrive.java
@@ -1,4 +1,14 @@
 package net.nosma;
 
 public class SimpleDotComGameTestDrive {
+    public static void main(String[] args) {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 4; y > 2; y--) {
+                System.out.println(x + " " + y);
+            }
+            if (x == 1) {
+                x++;
+            }
+        }
+    }
 }
